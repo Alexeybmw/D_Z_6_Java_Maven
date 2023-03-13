3ee83ebe0b1958bc043ea2c12b948065fa1e5658
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2023-03-13 11:29:04 +0300

    homework six,fixed bugs

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -5,68 +5,68 @@ import org.junit.jupiter.api.Test;
 
 public class StatsServiceTest {
     @Test
-    public void ShouldFindTheSumOfTheArray() {
+    public void shouldFindTheSumOfTheArray() {
         StatsService service = new StatsService();
 
         int[] array = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedSumaArray = 180;
-        int actualSumArray = service.Sumarray(array);
+        int actualSumArray = service.sumArray(array);
 
         Assertions.assertEquals(expectedSumaArray, actualSumArray);
     }
 
 
     @Test
-    public void ShouldFindAverageSalesPerMonth() {
+    public void shouldFindAverageSalesPerMonth() {
         StatsService service = new StatsService();
 
         int[] array = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedAveragеSalesAmount = 15;
-        int actualAveragеSales = service.AveragеSalesAmount(array);
+        int actualAveragеSales = service.averagеSalesAmount(array);
 
         Assertions.assertEquals(expectedAveragеSalesAmount, actualAveragеSales);
     }
 
     @Test
-    public void ShouldFindSalesForTheMaximumAmount() {
+    public void salesForTheMaxAmount() {
         StatsService service = new StatsService();
 
-        int[] array = {8, 15, 13, 15, 17, 20, 19,20, 7, 14, 14, 18};
-        int expectedSalesForTheMaximumAmount = 8;
-        int actualSalesForTheMax = service.SalesForTheMaximumAmount(array);
+        int[] array = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedSalesForTheMaxAmount = 8;
+        int actualSalesForTheMax = service.salesForTheMaxAmount(array);
 
-        Assertions.assertEquals(expectedSalesForTheMaximumAmount, actualSalesForTheMax);
+        Assertions.assertEquals(expectedSalesForTheMaxAmount, actualSalesForTheMax);
     }
 
     @Test
-    public void ShouldFindSalesForTheMinAmount() {
+    public void shouldFindSalesForTheMinAmount() {
         StatsService service = new StatsService();
 
         int[] array = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedSalesForTheMinAmount = 9;
-        int actualSalesForTheMin = service.SalesForTheMinAmount(array);
+        int actualSalesForTheMin = service.salesForTheMinAmount(array);
 
         Assertions.assertEquals(expectedSalesForTheMinAmount, actualSalesForTheMin);
     }
 
     @Test
-    public void ShouldFindLessThanAverageSalesMonths() {
+    public void shouldFindLessThanAverageSalesMonths() {
         StatsService service = new StatsService();
 
         int[] array = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedLessThanAverageSalesMonths = 5;
-        int actualLessThanAverageSalesMonths = service.LessThanAverageSalesMonths(array);
+        int actualLessThanAverageSalesMonths = service.lessThanAverageSalesMonths(array);
 
         Assertions.assertEquals(expectedLessThanAverageSalesMonths, actualLessThanAverageSalesMonths);
     }
 
     @Test
-    public void ShouldFindMonthsOfSalesAboveAverage() {
+    public void shouldFindMonthsOfSalesAboveAverage() {
         StatsService service = new StatsService();
 
         int[] array = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedFindMonthsOfSalesAboveAverage = 5;
-        int actualFindMonthsOfSalesAboveAverage = service.FindMonthsOfSalesAboveAverage(array);
+        int actualFindMonthsOfSalesAboveAverage = service.findMonthsOfSalesAboveAverage(array);
 
         Assertions.assertEquals(expectedFindMonthsOfSalesAboveAverage, actualFindMonthsOfSalesAboveAverage);
     }
